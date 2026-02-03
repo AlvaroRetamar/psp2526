d34c80e3918b6afa53f6a9de97f7ecb66ec4ea90
AlvaroRetamar <alvaro.delariva@retamar.es>
2026-02-03 13:10:42 +0100

    actualizar Lock en caso de multiprocesos

--- 8. Multihilo/Ejemplo Lock en procesos.py
+++ 8. Multihilo/Ejemplo Lock en procesos.py
@@ -1,22 +1,22 @@
 import multiprocessing
 import time
 
-def imprimir_letra(letra, repeticiones):
+def imprimir_letra(letra, repeticiones, candado):
     # Intentamos escribir una ráfaga de letras sin Lock
     for _ in range(5):
         # Creamos una cadena larga (ej. "AAAAA...")
         mensaje = letra * repeticiones
-        
-        print(f" INICIO {letra} -> " + mensaje + f" <- FIN {letra}\n")
+        with candado:
+            print(f" INICIO {letra} -> " + mensaje + f" <- FIN {letra}\n")
         time.sleep(0.01)
 
 if __name__ == "__main__":
     # Aumentamos el número de repeticiones para saturar el buffer
     n_repeticiones = 50000
-    
+    candado = multiprocessing.Lock()
     procesos = []
     for letra in ["A", "B", "C"]:
-        p = multiprocessing.Process(target=imprimir_letra, args=(letra, n_repeticiones))
+        p = multiprocessing.Process(target=imprimir_letra, args=(letra, n_repeticiones, candado))
         procesos.append(p)
         p.start()
 
